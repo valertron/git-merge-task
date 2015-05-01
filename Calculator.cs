c9ea6bc28b220570da7f0aaab4589c3a2f76e6fc
valertron <valertron@skbkontur.ru>
2015-05-01 11:32:58 +0500

    Rebase branch

--- Calculator.cs
+++ Calculator.cs
@@ -11,23 +11,32 @@ namespace Kontur.Courses.Git
 			if (args.Length == 0)
 				return lastResult;
 			if (args.Length == 1)
-				return lastResult = double.Parse(args[0]);
+				return lastResult = TryParseDouble(args[0]);
 			if (args.Length == 2)
 			{
 				double val;
-				var v2 = double.Parse(args[1]);
+				var v2 = TryParseDouble(args[1]);
 				if (!Double.TryParse(args[0], out val))
 				{
-					return lastResult = Execute(args[0], lastResult.Value, v2);
+					return lastResult = Execute(args[0], lastResult.Value, v2.Value);
 				}
 			}
 			if (args.Length == 3)
 			{
-				var v1 = double.Parse(args[0]);
-				var v2 = double.Parse(args[2]);
-				return lastResult = Execute(args[1], v1, v2);
+				var v1 = TryParseDouble(args[0]);
+				var v2 = TryParseDouble(args[2]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				return lastResult = Execute(args[1], v1.Value, v2.Value);
 			}
 			return Maybe<double>.FromError("Error input");
+		}
+
+		private Maybe<double> TryParseDouble(string s)
+		{
+			double v;
+			if (double.TryParse(s, out v))
+				return v;
+			return Maybe<double>.FromError("Not a number '{0}'", s);
 		}
 
 		private Maybe<double> Execute(string op, double v1, double v2)
